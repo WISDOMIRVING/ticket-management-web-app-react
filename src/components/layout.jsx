3bcaba8f7fafcb6fd854459e4a87e820bebaef30
WISDOM-IRVING <wisemandonstarirving@gmail.com>
2025-10-28 12:30:10 +0100

    Fixed bug

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './Header'
+import Header from './src\components\header.jsx'
 import Footer from './footer'
 
 export default function Layout({ children }) {
