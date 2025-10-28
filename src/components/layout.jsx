03e8a0b9c544ab39aba7bd9002b2c526afa9d910
WISDOM-IRVING <wisemandonstarirving@gmail.com>
2025-10-28 12:34:54 +0100

    Fix: corrected header/footer import paths

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Header from './src\components\header.jsx'
-import Footer from './footer'
+import Header from "./header.jsx";
+import Footer from "./footer.jsx";
+
 
 export default function Layout({ children }) {
   return (
